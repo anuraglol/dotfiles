9e468775f00e1f56d47595b016667fbd58ed4022
anurag <imanuraglol@gmail.com>
2025-02-18 22:32:02 +0530

    .

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # Add to ~/.config/fish/config.fish
+    set -x LANG en_US.UTF-8
+    set -x LC_ALL en_US.UTF-8
+    set -x GTK_IM_MODULE xim
+
+end
